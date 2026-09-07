ee87043c842fcfce0f68eca38b749816158f04ff
Einar96 <bergstrom40@gmail.com>
2026-09-07 10:38:34 +0200

    Connect wedding site to Apps Script backend

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// Den publika webbadressen till Google Apps Script Web App.
-// Inga gästkoder, mejladresser eller andra personuppgifter får läggas här.
-window.WEDDING_API_URL = '';
+// Publik webbadress till Google Apps Script Web App.
+// Inga gästkoder, mejladresser eller andra personuppgifter ska ligga här.
+window.WEDDING_API_URL = 'https://script.google.com/macros/s/AKfycbyEBp6GGQ55XouA_n-Hh3KY8WSRQ60u7Ft1Qct87OWXeX9frOjZDEtIQ939Nuqdeg/exec';
